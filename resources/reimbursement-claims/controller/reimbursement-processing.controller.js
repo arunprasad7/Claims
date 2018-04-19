a33b9398bc52d7ecbc962522a4bfe05fc1b5a5ca
arunprasad7 <arunprasad@minusculetechnologies.com>
2018-04-19 15:28:29 +0530

    reimbursment screen changes

--- resources/reimbursement-claims/controller/reimbursement-processing.controller.js
+++ resources/reimbursement-claims/controller/reimbursement-processing.controller.js
@@ -10,6 +10,8 @@
         function ReimbursmentProcessingController($scope, ReimbursementProcessingService, EclaimService, ngNotify) {
 
             $scope.claimsResult = [];
+            $scope.treatmentCodes = [];
+            $scope.rejectionCode = [];
 
             function init() {
                 $scope.claimsResult.push(createNewReimbursmentObject());
@@ -18,6 +20,8 @@
                 if(window.innerWidth >= 1300) {
                      $scope.noOfSlides = 4;
                 }
+                $scope.treatmentCodes = getCodes('T');
+                $scope.rejectionCodes = getCodes('R');
             }
 
             $scope.createNew = function() {
@@ -41,6 +45,23 @@
                     return (window.innerWidth - 380) + 'px';
                 }
                 return (window.innerWidth - 100) + 'px';
+            }
+
+            $scope.querySearch = function(query, codeType) {
+                if (codeType == 'R') {
+                    return query ? $scope.rejectionCodes.filter(createFilterFor(query)) : $scope.rejectionCodes;
+                } else {
+                    return query ? $scope.treatmentCodes.filter(createFilterFor(query)) : $scope.treatmentCodes;
+                }
+            }
+
+            function createFilterFor(query) {
+                var lowercaseQuery = angular.lowercase(query);
+    
+                return function filterFn(state) {
+                    return (((angular.lowercase(state.name).indexOf(lowercaseQuery) != 0) && angular.lowercase(state.name).indexOf(lowercaseQuery) != -1) || (angular.lowercase(state.name).indexOf(lowercaseQuery) === 0));
+                };
+    
             }
 
             function createNewReimbursmentObject() {
@@ -73,6 +94,27 @@
                     {'reqNum': 487896, 'claimStatus':'rejected', 'statusReason':'Waiting for Approval', 'rejectedReason':'Waiting for Finalization'},
                     {'reqNum': 587896, 'claimStatus':'rejected', 'statusReason':'Waiting for Approval', 'rejectedReason':'Waiting for Finalization'},
                     {'reqNum': 687896, 'claimStatus':'rejected', 'statusReason':'Waiting for Approval', 'rejectedReason':'Waiting for Finalization'}
+                ];
+            }
+
+            function getCodes(codeType) {
+                return [
+                    {
+                        'code' : 1111,
+                        'name' : codeType + ' Code 1' 
+                    },
+                    {
+                        'code' : 2222,
+                        'name' : codeType + ' Code 2' 
+                    },
+                    {
+                        'code' : 3333,
+                        'name' : codeType + ' Code 3' 
+                    },
+                    {
+                        'code' : 4444,
+                        'name' : codeType + ' Code 4' 
+                    }
                 ];
             }
 
