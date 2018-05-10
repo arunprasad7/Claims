3dfad0b3a87d3d900d572b52c514731e4ded8972
arunprasad7 <arunprasad@minusculetechnologies.com>
2018-05-10 16:54:59 +0530

    custom pagination included

---
+++ resources/util/pagination/pagination.directive.js
@@ -0,0 +1,84 @@
+(function() {
+    'use strict';
+
+    angular
+        .module("claims")
+            .directive("pagination", function($rootScope, PaginationFactory) {
+                return {
+                    restrict: 'E',
+                    templateUrl: 'resources/util/pagination/pagination.html',
+                    scope: {
+                        recordsToDisplay: '@',
+                        records: '=',
+                        totalRecordCount: '@'
+                    },
+                    link: function(scope, element, attrs) {
+                        scope.pager = {};
+
+                        scope.setPage = function(page) {
+                            if (page < 1 || page > scope.pager.totalPages) {
+                                return;
+                            }
+                            scope.pager = PaginationFactory.getPageSettings(scope.totalRecordCount, page);
+                            scope.items = scope.records.slice(scope.pager.startIndex, scope.pager.endIndex + 1);
+                        }
+
+                        scope.$watch('records', function(newValue, oldValue, scope) {
+                            init();
+                        })
+
+                        function init() {
+                            scope.setPage(1);
+                        }
+
+                        init();
+                    }
+                }
+            }
+        )
+        .factory("PaginationFactory", function() {
+            var service = {};
+            service.getPageSettings = getPageSettings;
+            return service;
+    
+            function getPageSettings(totalItems, currentPage, pageSize) {
+                currentPage = currentPage || 1;
+                pageSize = pageSize || 10;
+                var totalPages = Math.ceil(totalItems / pageSize);
+
+                var startPage, endPage;
+                if (totalPages <= 10) {
+                    startPage = 1;
+                    endPage = totalPages;
+                } else {
+                    if (currentPage <= 6) {
+                        startPage = 1;
+                        endPage = 10;
+                    } else if (currentPage + 4 >= totalPages) {
+                        startPage = totalPages - 9;
+                        endPage = totalPages;
+                    } else {
+                        startPage = currentPage - 5;
+                        endPage = currentPage + 4;
+                    }
+                }
+
+                var startIndex = (currentPage - 1) * pageSize;
+                var endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
+                var pages = _.range(startPage, endPage + 1);
+
+                return {
+                    totalItems: totalItems,
+                    currentPage: currentPage,
+                    pageSize: pageSize,
+                    totalPages: totalPages,
+                    startPage: startPage,
+                    endPage: endPage,
+                    startIndex: startIndex,
+                    endIndex: endIndex,
+                    pages: pages
+                };
+            }
+        }
+    );
+})()
