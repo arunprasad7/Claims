64735546d066a20089e0635c99f8ccea6509ae0e
arunprasad7 <arunprasad@minusculetechnologies.com>
2018-05-11 15:09:43 +0530

    claims list view directive created

---
+++ resources/shared/claimsListView-directive/claimslistview.directive.service.js
@@ -0,0 +1,21 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('claims')
+        .service('ClaimsListViewService', ClaimsListViewService);
+
+        function ClaimsListViewService() {
+
+            this.getTabsToDisplay = function() {
+                return [
+                    {'label': 'New Request', 'tab' : 'newRequest', 'state': 'New Request'},
+                    {'label': 'In Progress', 'tab' : 'InProgress', 'state': 'InProgress'},
+                    {'label': 'Approved', 'tab' : 'Approved', 'state': 'Approved'},
+                    {'label': 'Waiting for approval', 'tab' : 'Waitingforapproval', 'state': 'Waitingforapproval'},
+                    {'label': 'Rejected', 'tab' : 'Rejected', 'state': 'Rejected'},
+                    {'label': 'Assigned', 'tab' : 'Assigned', 'state': 'Assigned'}
+                ];
+            }
+        }
+})();
